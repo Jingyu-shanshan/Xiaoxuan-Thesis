0e639614735ab451bc555d84b3afc9e1ade8661b
jy-shanshan <jingyu.liushanshan@outlook.com>
2023-02-08 15:13:40 +0800

    add s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+resource "aws_s3_bucket" "s3" {
+  bucket = var.bucket_name
+  region = var.AWS_REGION
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
 resource "aws_instance" "my_vm" {
   ami           = "ami-065deacbcaac64cf2"
   instance_type = "t2.micro"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "AWS_REGION" {
   type    = string
   default = "eu-north-1"
 }
+
+variable "bucket_name" {
+  type = string
+  default = "tf-tuts-state"
+}
