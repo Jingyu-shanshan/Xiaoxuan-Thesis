94246d9a6d695a608524baa4be5037e33dd5cb56
jy-shanshan <jingyu.liushanshan@outlook.com>
2023-02-08 15:40:42 +0800

    invalid region in aws_instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "my_vm" {
   ami           = "ami-065deacbcaac64cf2"
   instance_type = "t2.micro"
-  region        = var.AWS_REGION
 
   tags = {
     Name = "My EC2 instance",
