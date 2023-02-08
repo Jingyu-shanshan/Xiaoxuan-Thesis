0224c98ebc167e879b6ab5f36fd9359b1b2ee3fd
jy-shanshan <jingyu.liushanshan@outlook.com>
2023-02-08 14:42:32 +0800

    bug fix: invalid variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,7 @@
-provider "aws" {
-  region = var.AWS_REGION
-}
-
 resource "aws_instance" "my_vm" {
   ami           = "ami-065deacbcaac64cf2"
   instance_type = "t2.micro"
+  region = var.AWS_REGION
 
   tags = {
     Name = "My EC2 instance",
