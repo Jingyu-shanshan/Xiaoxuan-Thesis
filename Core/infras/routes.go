8f8a9881c1fe87c90e268c927c3b132b996861b0
jy-shanshan <jingyu.liushanshan@outlook.com>
2023-02-10 14:33:38 +0800

    feat: export the func

--- Core/infras/routes.go
+++ Core/infras/routes.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func setupRoutes(app *fiber.App) {
+func SetupRoutes(app *fiber.App) {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("This is first route")
 	})
