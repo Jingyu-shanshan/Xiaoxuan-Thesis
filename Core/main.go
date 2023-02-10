1bfddb106b5b4bd6e8cd63d9a7402d678e5363ba
jy-shanshan <jingyu.liushanshan@outlook.com>
2023-02-10 11:23:13 +0800

    feat: connect to postgresql database

--- Core/main.go
+++ Core/main.go
@@ -1,13 +1,35 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	app := fiber.New()
+	var host, user, password, database string = "db", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME")
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	db, err := sql.Open(
+		"postgres",
+		fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, user, password, database),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully created connection to database")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello Xiaoxuan!")
 	})
 
-	app.Listen(":8082")
+	log.Fatal(http.ListenAndServe(":8082", nil))
 }
